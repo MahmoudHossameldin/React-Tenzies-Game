874679b09573f0b87e274b32ad5da2834480d37f
MahmoudHossameldin <mahmoud.ahmed.aw@gmail.com>
2022-03-19 15:06:17 +0200

    added base files and code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Die from "./Die";
+import "./index.css";
+
+function App() {
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+    </main>
+  );
+}
+
+export default App;
