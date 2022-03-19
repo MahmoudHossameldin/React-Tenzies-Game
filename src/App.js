a6cc17d89306458f957f79b496f1e7b09a495ddb
MahmoudHossameldin <mahmoud.ahmed.aw@gmail.com>
2022-03-19 17:36:14 +0200

    finish game: added New Game functionality

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export default function App() {
     const allSameValue = dice.every((die) => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
-      console.log("You won!");
     }
   }, [dice]);
 
@@ -35,11 +34,16 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice((oldDice) =>
-      oldDice.map((die) => {
-        return die.isHeld ? die : generateNewDie();
-      })
-    );
+    if (!tenzies) {
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
   }
 
   function holdDice(id) {
