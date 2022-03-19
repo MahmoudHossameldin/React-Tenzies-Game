47f4b5ddd442d6d85583108dcd19629dfac387c0
MahmoudHossameldin <mahmoud.ahmed.aw@gmail.com>
2022-03-19 16:58:01 +0200

    added the functionality to Roll the dice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+
 import Die from "./Die";
 import "./index.css";
 
-function App() {
+export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
-
-export default App;
